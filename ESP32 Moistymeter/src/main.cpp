015a8969220022aa926a35d6cdf237f1c3c2df4a
Vid Furlan <nalrufdiv@gmail.com>
2023-04-04 22:20:36 +0200

    JSON starting file

--- ESP32 Moistymeter/src/main.cpp
+++ ESP32 Moistymeter/src/main.cpp
@@ -1,120 +1,85 @@
-// https://randomnerdtutorials.com/esp32-web-server-arduino-ide/
-#include <Arduino.h>
+/*
+  Rui Santos
+  Complete project details at Complete project details at https://RandomNerdTutorials.com/esp32-http-get-post-arduino/
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
 #include <WiFi.h>
+#include <HTTPClient.h>
 
-const char *ssid = "VidPixel";
-const char *password = "12345678";
+const char* ssid = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
 
-// Set web server port number to 80
-WiFiServer server(80);
+//Your Domain name with URL path or IP address with path
+const char* serverName = "http://192.168.1.106:1880/update-sensor";
 
-// Variable to store the HTTP request
-String header;
-
-float photoresistorVar = 0.0f;
-
-const int photoresistorPin = 27;
-
-// Current time
-unsigned long currentTime = millis();
-// Previous time
-unsigned long previousTime = 0;
-// Define timeout time in milliseconds (example: 2000ms = 2s)
-const long timeoutTime = 2000;
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastTime = 0;
+// Timer set to 10 minutes (600000)
+//unsigned long timerDelay = 600000;
+// Set timer to 5 seconds (5000)
+unsigned long timerDelay = 5000;
 
 void setup() {
   Serial.begin(115200);
 
-  pinMode(photoresistorPin, INPUT);
-
-  // Connect to Wi-Fi network with SSID and password
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-
-  // Print local IP address and start web server
   Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
+  Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
-  server.begin();
+ 
+  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
 }
 
 void loop() {
-  WiFiClient client = server.available(); // Listen for incoming clients
+  //Send an HTTP POST request every 10 minutes
+  if ((millis() - lastTime) > timerDelay) {
+    //Check WiFi connection status
+    if(WiFi.status()== WL_CONNECTED){
+      WiFiClient client;
+      HTTPClient http;
+    
+      // Your Domain name with URL path or IP address with path
+      http.begin(client, serverName);
+      
+      // If you need Node-RED/server authentication, insert user and password below
+      //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
+      
+      // Specify content-type header
+      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+      // Data to send with HTTP POST
+      String httpRequestData = "api_key=tPmAT5Ab3j7F9&sensor=BME280&value1=24.25&value2=49.54&value3=1005.14";           
+      // Send HTTP POST request
+      int httpResponseCode = http.POST(httpRequestData);
+      
+      // If you need an HTTP request with a content type: application/json, use the following:
+      //http.addHeader("Content-Type", "application/json");
+      //int httpResponseCode = http.POST("{\"api_key\":\"tPmAT5Ab3j7F9\",\"sensor\":\"BME280\",\"value1\":\"24.25\",\"value2\":\"49.54\",\"value3\":\"1005.14\"}");
 
-  if (client) { // If a new client connects,
-    currentTime = millis();
-    previousTime = currentTime;
-    Serial.println("New Client."); // print a message out in the serial port
-    String currentLine =
-        ""; // make a String to hold incoming data from the client
-    while (client.connected() &&
-           currentTime - previousTime <=
-               timeoutTime) { // loop while the client's connected
-      char c = client.read(); // read a byte, then
-      Serial.write(c);        // print it out the serial monitor
-      header += c;
-      if (c == '\n') { // if the byte is a newline character
-        // if the current line is blank, you got two newline characters in a
-        // row. that's the end of the client HTTP request, so send a response:
-        if (currentLine.length() == 0) {
-          // HTTP headers always start with a response code (e.g. HTTP/1.1 200
-          // OK) and a content-type so the client knows what's coming, then a
-          // blank line:
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-type:text/html");
-          client.println("Connection: close");
-          client.println();
-
-          // Display the HTML web page
-          client.println("<!DOCTYPE html><html>");
-          client.println("<head><meta name=\"viewport\" "
-                         "content=\"width=device-width, initial-scale=1\">");
-          client.println("<link rel=\"icon\" href=\"data:,\">");
-          // CSS to style the on/off buttons
-          // Feel free to change the background-color and font-size attributes
-          // to fit your preferences
-          client.println(
-              "<style>html { font-family: Helvetica; display: inline-block; "
-              "margin: 0px auto; text-align: center;}");
-          client.println(".button { background-color: #4CAF50; border: none; "
-                         "color: white; padding: 16px 40px;");
-          client.println("text-decoration: none; font-size: 30px; margin: 2px; "
-                         "cursor: pointer;}");
-          client.println(
-              ".button2 {background-color: #555555;}</style></head>");
-
-          // Web Page Heading
-          client.println("<body><h1>Lightomer 5005 Server</h1>");
-          char buffer[50];
-          snprintf(buffer, sizeof(buffer), "<p>Shinystat - State %2.4f </p>",
-                   photoresistorVar);
-          client.println(buffer);
-          client.println("</body></html>");
-          Serial.println(buffer);
-
-          // The HTTP response ends with another blank line
-          client.println();
-          // Break out of the while loop
-          break;
-        } else { // if you got a newline, then clear currentLine
-          currentLine = "";
-        }
-      } else if (c != '\r') { // if you got anything else but a carriage return
-                              // character,
-        currentLine += c;     // add it to the end of the currentLine
-      }
+      // If you need an HTTP request with a content type: text/plain
+      //http.addHeader("Content-Type", "text/plain");
+      //int httpResponseCode = http.POST("Hello, World!");
+     
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+        
+      // Free resources
+      http.end();
     }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
   }
-  // Clear the header variable
-  header = "";
-  // Close the connection
-  client.stop();
-  Serial.println("Client disconnected.");
-  Serial.println("");
 }
