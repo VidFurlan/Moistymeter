d8db74cd41dd1be8f81c89b4c8129336fc25bc03
Vid Furlan <nalrufdiv@gmail.com>
2023-06-07 15:00:58 +0200

    Sendiing data over ESP NOW works

--- Hardware/ESP32 Server/src/main.cpp
+++ Hardware/ESP32 Server/src/main.cpp
@@ -1,37 +1,66 @@
-#include <HTTPClient.h>
 #include <WiFi.h>
 #include <esp_now.h>
 #include <iostream>
 
-const uint8_t receiverAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+//MAC Addr for the client
+const uint8_t receiverAddress[] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
 esp_now_peer_info_t peerInfo;
 
-typedef struct struct_message 
+typedef struct struct_message
 {
-    String message;
+  int id = 0; // 0 for master
+  String message;
 } struct_message;
 
 struct_message data_to_send;
 
-void setup()
-{
-    data_to_send.message = "Hello Friend";
-
-    Serial.begin(115200);
-    WiFi.mode(WIFI_STA);
-    for (int i=0; i<sizeof(receiverAddress); i++) {
-        peerInfo.peer_addr[i] = receiverAddress[i];
-        Serial.println(peerInfo.peer_addr[i]);
-    }
-    
-
-    esp_now_init();
-    esp_now_add_peer(&peerInfo);
-    esp_now_send(receiverAddress, (uint8_t *) &data_to_send, sizeof(data_to_send));
-
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Successful" : "Delivery Failed");
 }
 
-void loop() 
+static void esp_now_setup()
 {
+  WiFi.mode(WIFI_STA);
 
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Didn't initializr ESP NOW");
+    return;
+  }
+
+  esp_now_register_send_cb(OnDataSent);
+
+  // Move the receaver address to the peer info var
+  for (int i = 0; i < sizeof(receiverAddress); i++) {
+    peerInfo.peer_addr[i] = receiverAddress[i];
+    Serial.println(peerInfo.peer_addr[i]);
+  }
+  peerInfo.encrypt = false;
+
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+    Serial.println("Failed to add peers");
+    return;
+  }
+}
+
+void setup()
+{
+  data_to_send.message = "Hello Friend";
+
+  Serial.begin(115200);
+
+  esp_now_setup();
+}
+
+void loop()
+{
+  esp_err_t result = esp_now_send(receiverAddress, (uint8_t*)&data_to_send, sizeof(data_to_send));
+
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+  delay(5000);
 }
--- README.md
+++ README.md
@@ -11,4 +11,7 @@ Personal project/system for monitoring your plant's soil moisture, the light lev
 
 ## People:
 - [**Vid Furlan**](https://github.com/VidFurlan)
-- [**Nik** Reducted](https://github.com/WaterFlown)
+- [**Nik** Reducted](https://github.com/WaterFlown) Made 1 Commit (Not joking)
+
+## Resources
+https://randomnerdtutorials.com/esp-now-many-to-one-esp32/
